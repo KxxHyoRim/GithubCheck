32bea39961cd3b2330bbafc94962e144cfd999c7
KxxHyoRim <rlagyfla0709@gmail.com>
2021-10-18 23:55:02 +0900

    Add Accounts

--- github_check.py
+++ github_check.py
@@ -12,10 +12,9 @@ from selenium.webdriver.support import expected_conditions as EC
 git_home = 'https://github.com/'
 login = 'https://github.com/login?return_to=https%3A%2F%2Fgithub.com%2FKxxHyoRim'
 git_page = 'https://github.com/' # 뒤에 아이디 붙여서 사용
-git_id = ['KxxHyoRim', 'yeseul106']
+git_id = ['KxxHyoRim', 'yeseul106', 'HoyeonYu', 'LeeHyewon225']
 
 def isUserExist(id):
-    
     # id로 검색
     driver.get('https://github.com/search?q=' + id + '&type=users')
     time.sleep(1)
@@ -36,24 +35,24 @@ def getOnePersonInfo(id):
     name_xpath = '//*[@id="js-pjax-container"]/div[2]/div/div[1]/div/div[2]/div[1]/div[2]/h1/span[1]'
     id_xpath = '//*[@id="js-pjax-container"]/div[2]/div/div[1]/div/div[2]/div[1]/div[2]/h1/span[2]'
 
+    print("*******************")
 
-    ''' 이름가져오기 : 없는 사람도 있음 ex) 슬예조'''
+    ''' 이름가져오기 : 설정 안해둔 사람도 있으므로 우선 주석처리 '''
     # try :
-    #     print("*** name ***")
-    #     print(driver.find_element_by_xpath(name_xpath).text)
+    #     print( driver.find_element_by_xpath(name_xpath).text)
     # except: # 이름 지정 안되어 있음
     #     pass
+
 
     ''' 아이디 가져오기 '''
-    # try :
-    #     print("*** ID ***")
-    #     print(driver.find_element_by_xpath(id_xpath).text)
-    # except: # 이름 지정 안되어 있음
-    #     pass
+    try :
+        print('ID : ',driver.find_element_by_xpath(id_xpath).text)
+    except: # 이름 지정 안되어 있음
+        pass
 
-    '''Commit : class명으로 가져오기'''
+
+    ''' Commit 수 가져오기 : class 속성 사용 '''
     # Commit : css, xpath 사람마다 다름 -> 불가능
-
     today = str(date.today())                                               # 오늘 날짜
     rect = driver.find_elements_by_class_name('ContributionCalendar-day')   # 잔디 정보 전체 
 
@@ -75,9 +74,11 @@ if __name__ == "__main__":
     options.add_argument('--no-sandbox')
     driver = webdriver.Chrome('C:\chromedriver_win32\chromedriver', chrome_options=options)
 
-    for person in git_id:
-        if (isUserExist(person)):
-            getOnePersonInfo(person)
-        else:
-            print('user not exist ')
+    # for person in git_id:
+    #     if (isUserExist(person)):
+    #         getOnePersonInfo(person)
+    #     else:
+    #         print('user not exist ')
 
+    for person in git_id:
+        getOnePersonInfo(person)
